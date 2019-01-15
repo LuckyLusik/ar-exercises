fc747b81b24c0d69208c7c09433aed0fd2de37f8
Lucy <lucyneatdesign@yahoo.ca>
2019-01-15 21:22:29 +0000

    All done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mike", last_name: "Tomas", hourly_rate: 70)
+@store1.employees.create(first_name: "Nick", last_name: "Berg", hourly_rate: 60)
+@store1.employees.create(first_name: "Chris", last_name: "Nesh", hourly_rate: 55)
+@store2.employees.create(first_name: "Tomas", last_name: "Moon", hourly_rate: 60)
+@store2.employees.create(first_name: "Alfred", last_name: "Hichckok", hourly_rate: 65)
+@store2.employees.create(first_name: "Vlad", last_name: "Nabokov", hourly_rate: 100)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
 
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_at_least_one_of_the_men_or_women_apparel
+  has_many :employees
+
+  def must_carry_at_least_one_of_the_men_or_women_apparel
+    if mens_apparel == nil && womens_apparel == nil
+      errors.add(:mens_apparel, "must carry at least one of the men's or women's apparel")
+    elsif mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "must carry at least one of the men's or women's apparel")
+    end
+  end
+
+
 end
